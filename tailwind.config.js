31bda1861b1257b52ed8621948a9c39c0db845df
Kirill Titov <volos88@gmail.com>
2022-07-14 23:03:50 +0300

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    spacing: {
+      '100%': '100%',
+      '5': '5px',
+      '10': '10px',
+      '15': '15px',
+      '20': '20px',
+      '25': '25px',
+      '30': '30px',
+      '35': '35px',
+      '40': '40px',
+      '45': '45px',
+      '50': '50px',
+      '55': '55px',
+      '60': '60px',
+      '65': '65px',
+      '70': '70px',
+      '75': '75px',
+      '80': '80px',
+      '85': '85px',
+      '90': '90px',
+      '95': '95px',
+      '100': '100px',
+    },
+    container: {
+      center: true,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
