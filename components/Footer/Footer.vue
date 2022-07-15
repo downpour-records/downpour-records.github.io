2ecd35652f36f424d0f61d2aab5efb56f212d436
Kirill Titov <volos88@gmail.com>
2022-07-15 22:49:31 +0300

    separate vue and scss files

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,18 +1,14 @@
+<script>
+import "@/components/Footer/Footer.scss"
+
+export default {
+  name: 'Footer'
+}
+</script>
+
 <template>
   <footer>
     <div class="container">
     </div>
   </footer>
 </template>
-
-<style lang="scss">
-footer {
-  @apply py-20px;
-}
-</style>
-
-<script>
-export default {
-  name: 'Footer'
-}
-</script>
--- components/Header/Header.vue
+++ components/Header/Header.vue
@@ -1,3 +1,11 @@
+<script>
+import "@/components/Header/Header.scss"
+
+export default {
+  name: 'Header'
+}
+</script>
+
 <template>
   <header>
     <div class="container">
@@ -7,23 +15,3 @@
     </div>
   </header>
 </template>
-
-<style lang="scss">
-header {
-  background: rgb(0 30 38 / 80%);
-  border-bottom: 1px solid #203247;
-  padding: 16px 0;
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  width: 100%;
-  backdrop-filter: blur(12px);
-}
-</style>
-
-<script>
-export default {
-  name: 'Header'
-}
-</script>
