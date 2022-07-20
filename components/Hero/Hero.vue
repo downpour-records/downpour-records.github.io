6d3e4e5b10322bb609ac1f3097de72b130385f96
Kirill Titov <volos88@gmail.com>
2022-07-20 10:34:07 +0300

    test

--- components/Hero/Hero.vue
+++ components/Hero/Hero.vue
@@ -6,14 +6,36 @@
       <img src="~/assets/images/hero/hero_bg.jpg" class="hero__bg lazyload" alt="Hero image">
     </picture>
     <div class="container flex items-center flex-col justify-center h-full relative z-10">
-      <h1 class="text-7xl text-white font-medium mb-5">Downpour records</h1>
+      <h1 class="text-7xl text-white font-medium mb-5">Downpour records123</h1>
       <p class="text-lightgrey text-2xl font-medium">Let the world hear you</p>
     </div>
   </section>
 </template>
 
+<style lang="scss">
+.hero {
+  overflow: hidden;
+  width: 100%;
+  height: 100vh;
+
+  @apply bg-secondary;
+  @apply relative;
+
+  &__bg {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+
+    @apply absolute;
+    @apply top-0;
+    @apply left-0;
+  }
+}
+</style>
+
 <script>
-import "@/components/Hero/Hero.scss"
+// import "@/components/Hero/Hero.scss"
 
 export default {
   name: 'Hero',
